a8a3b8b5fbc1db1bbc5d24b53bc1abe664751ebd
Celso Pinheiro <cjpinheiro2@outlook.com>
2022-01-25 09:33:17 -0300

    Filtros

--- Frontend/CjEvents-App/src/app/events/events.component.ts
+++ Frontend/CjEvents-App/src/app/events/events.component.ts
@@ -9,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class EventsComponent implements OnInit {
 
   public events: any = [];
+  public filteredEvents: any = [];
   widthImg : number = 150;
   marginImg : number = 2;
   showImage : boolean = true;
+  _filterRows : string = "";
+
+  public get filterRows(){
+    return this._filterRows
+  }
+  public set filterRows(value){
+    this._filterRows = value;
+    this.filteredEvents = this._filterRows ? this.FilterEvents(this._filterRows) : this.events;
+  }
+
   constructor(private http: HttpClient) { }
 
 
@@ -26,8 +37,21 @@ export class EventsComponent implements OnInit {
   }
   public getEventos() : void {
     this.http.get('https://localhost:5001/api/Event').subscribe(
-      response => this.events = response,
+      response => {
+        this.events = response,
+        this.filteredEvents = response
+      },
       error => console.log(error)
     );
+  }
+  public FilterEvents(filter : string) : any {
+    filter = filter.toLocaleLowerCase();
+
+    return this.events.filter(
+      (event : any) => event.theme.toLocaleLowerCase().indexOf(filter) !== -1 ||
+      event.place.toLocaleLowerCase().indexOf(filter) !== -1
+    )
+
+
   }
 }
